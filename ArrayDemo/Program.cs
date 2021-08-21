9988a6adb0b7b5b4ad593dc13563d99045543f68
桂素伟 <axzxs2001@163.com>
2021-08-21 20:45:07 +0900

    Console.Array

---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿//var arr = new string[] { "a", "b", "c", "d" };
+
+//var newArr = arr[..2];
+//for (int i = 0; i < newArr.Length; i++)
+//{
+//    Console.WriteLine(newArr[i]);
+//}
+var arr = new string[] { "b", "c", "a", "d" };
+Array.Sort(arr);
+Array.Reverse(arr);
+foreach (var e in arr)
+{
+    Console.WriteLine(e);
+}
+
+//var arr1 = new string[,] {
+//    {"a1","a2","a3" },
+//    {"b2","b2","b3" }
+//};
+//for (var r = 0; r < arr1.GetLength(0); r++)
+//{
+//    for (var c = 0; c < arr1.GetLength(1); c++)
+//    {
+//        Console.WriteLine(arr1[r, c]);
+//    }
+//    Console.WriteLine("-------");
+//}
+
+//var arr = new string[2][] {
+//new string[]{"a1","a2"},
+//new string[]{"b1","b2","b3"},
+//};
+
+//for (var r = 0; r < arr.Length; r++)
+//{
+//    for (var c = 0; c < arr[r].Length; c++)
+//    {
+//        Console.WriteLine(arr[r][c]);
+//    }
+//    Console.WriteLine("-------");
+//}
---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Console.Write("abcd");
+//Console.WriteLine("Hello, World!");
+
+//var content = Console.ReadLine();
+//Console.WriteLine(content);
+
+//var i = Console.Read();
+//var i1 = Console.Read();
+//var i2 = Console.Read();
+//var i3 = Console.Read();
+//Console.WriteLine((char)i);
+//Console.WriteLine(i1);
+//Console.WriteLine(i2);
+//Console.WriteLine(i3);
+
+//var password = "";
+//while (true)
+//{
+//    var key = Console.ReadKey(true);
+//    Console.Write("*");
+//    password += key.KeyChar;
+//    if (key.KeyChar == 13)
+//    {
+//        break;
+//    }
+//}
+//Console.WriteLine();
+//Console.WriteLine($"password:{password}");
